5db4f7f2f8bdbfdbaa4902d93877e2565b585dbd
Colin-Cai-jin <45095757+Colin-Cai-jin@users.noreply.github.com>
2019-02-08 22:33:10 +0800

    Create term.c

---
+++ term.c
@@ -0,0 +1,55 @@
+#include <termio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <poll.h>
+#include <errno.h>
+
+struct termios t_old;
+
+/*Set the terminal back when the process exited*/
+void do_exit(void)
+{
+        tcsetattr(STDIN_FILENO, TCSANOW, &t_old);
+}
+
+int main()
+{
+        char c;
+        int ret;
+        struct termios t;
+        struct sigaction sa;
+        struct pollfd pf[2];
+
+        sa.sa_handler = SIG_IGN;
+        sa.sa_flags = 0;
+        sigemptyset(&sa.sa_mask);
+        sigaction(SIGPIPE, &sa, NULL);
+
+        tcgetattr(STDIN_FILENO, &t);
+        memcpy(&t_old, &t, sizeof(struct termios));
+        atexit(do_exit);
+        cfmakeraw(&t);
+        tcsetattr(STDIN_FILENO, TCSANOW, &t);
+
+        while(1) {
+                pf[0].fd = STDIN_FILENO;
+                pf[0].events = POLLIN;
+                pf[1].fd = STDOUT_FILENO;
+                pf[1].events = 0;
+                if(poll(pf,2,-1)>0) {
+                        if(pf[1].revents & POLLERR)
+                                break;
+                }
+                ret = read(STDIN_FILENO, &c, 1);
+                if(ret == 0)
+                        break;
+                if(ret < 0)
+                        continue;
+                if(write(STDOUT_FILENO, &c, 1) == 1)
+                        ;
+        }
+        return 0;
+}
