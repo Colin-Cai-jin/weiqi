5b25bf5c8c628947fa93207312e750163de8c7ab
Colin-Cai-jin <45095757+Colin-Cai-jin@users.noreply.github.com>
2019-02-08 22:15:52 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-make
+
+#You can use Makefile here. Or, you can modify the program as you want.
+[ -e ./input ] || cc ./term.c -o ./input
+[ -e ./weiqi ] || raco exe -o ./weiqi weiqi.rkt
+
+#Run
 ./input | ./weiqi
